3b6c1621a6499dd93e0cec0b06239c0a655789cc
AdeSam97 <adejaresamson42@gmail.com>
2025-05-09 14:54:42 +0100

    update

--- install-setup.sh
+++ install-setup.sh
@@ -8,15 +8,24 @@ if aws --version &>/dev/null; then
    exit 0
 else
    echo "⚠️ AWS CLI is not installed. let's set up"
+   sudo apt update -y
+   sudo apt install -y curl unzip
+   curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+   unzip awscliv2.zip
+   sudo ./aws/install   #install awscli
+   aws --version
+   rm -rf awscliv2.zip aws
 fi
-
-sudo apt update -y
-sudo apt install -y curl unzip
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install   #install awscli
-aws --version
 rm -rf awscliv2.zip aws
+
+echo "Let's check if you've configured aws-cli"
+if aws sts get-caller-identity &>/dev/null; then
+   echo "✅ AWS CLI is already configured."
+   aws sts get-caller-identity
+   exit 0
+else
+   echo "⚠️ AWS CLI is not configured. Let's configure!"
+fi
 #take input for secret and access key
 read -p "Enter your AWS Access Key ID: " AWS_ACCESS_KEY_ID
 read -p "Enter your AWS Secret Access Key: " AWS_SECRET_ACCESS_KEY
